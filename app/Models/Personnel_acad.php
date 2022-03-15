900d7638bb3c6e16bed5a58d2a6fa167dbd3a7ec
Wassim MEYYAH <wassim.meyyah@gmail.com>
2022-03-15 17:55:36 +0100

    Nordine

--- app/Models/Personnel_acad.php
+++ app/Models/Personnel_acad.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Peronnel_acad extends Model
+class Personnel_acad extends Model
 {
     /**
      * @return string
