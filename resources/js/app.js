0ced604f9b8cb4dcd5006f1d78c25319e6872ae3
N.Bouskine <n.bouskine@eleve.leschartreux.net>
2022-03-21 18:38:14 +0100

    ajout cartographie etablissement

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
 require('./bootstrap');
+
+window.Vue = require('vue').default;
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+import * as VueGoogleMaps from 'vue2-google-maps';
+
+Vue.use(VueGoogleMaps, {
+    load: {
+        key: ''
+    }
+
+})
+
+const app = new Vue({
+    el: '#app',
+});
